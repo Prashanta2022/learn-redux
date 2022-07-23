3c7fd7e20faa23077d848fd6fe3be0a3c3c1de59
Prashanta2022 <ofa.prashanta@gmail.com>
2022-07-23 16:20:59 +0900

    add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { legacy_createStore as createStore } from 'redux';
+
+//let store = createStore(reducer);
+
+//STORE -> GLOBALIZED STORE
+
+//ACTION INCREMENT
+
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
+const decrement = () => {
+  return {
+    type: 'DECREMENT',
+  };
+};
+
+//REDUCER -> how action will behave, update
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+//DISPATCH it in the console
+
+store.subscribe(() => console.log(store.getStore()));
+
+//DISPATCH -> execute action
+
+store.dispatch(increment());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
